48b58aa66a3199463484d2a34d4d8fd52c529bf5
Rajesh <rajeshrajatv@gmail.com>
2023-02-18 10:31:46 +0530

    Add python code

---
+++ server.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Hello, World!')
+
+
+httpd = HTTPServer(('localhost', 8000), Handler)
+httpd.serve_forever()
